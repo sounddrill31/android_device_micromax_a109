44354a30d4aa06869acc3d45198c05003129c983
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-05-12 20:14:39 +0530

    Update BoardConfig.mk

--- recovery/BoardConfig.mk
+++ recovery/BoardConfig.mk
@@ -31,9 +31,9 @@ TW_INCLUDE_CRYPTO := true                     # true/false: Add Data Encryption 
 TW_INPUT_BLACKLIST := "hbtp_vm"               # Optional: Disables virtual mouse
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_hdpi                     # Set the exact theme you wanna use. If resulation doesn't match, define the height/width
-DEVICE_RESOLUTION := 720x1280                 # The Resolution of your Device
-TARGET_SCREEN_HEIGHT := 1280                    # The height
-TARGET_SCREEN_WIDTH := 720                      # The width
+DEVICE_RESOLUTION := 540x960                 # The Resolution of your Device
+TARGET_SCREEN_HEIGHT := 960                    # The height
+TARGET_SCREEN_WIDTH := 540                      # The width
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 # Set the Brightness Control File Path below (as per your chip/device)
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
