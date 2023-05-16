ab07196d926b9c9be28fd2d4e8329c305349a093
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-05-16 09:29:48 +0530

    Update custombootimg.mk

--- custombootimg.mk
+++ custombootimg.mk
@@ -1,7 +1,7 @@
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(recovery_ramdisk)
 	@echo ----- Making recovery image ------
-	perl device/micromax/A109/repack-MT65xx.pl -recovery device/micromax/A109/kernel out/target/product/A109/recovery/root $@ $(MKBOOTIMG)
+	perl device/micromax/A109/repack-MT65xx.pl -recovery device/micromax/A109/prebuilt/kernel out/target/product/A109/recovery/root $@ $(MKBOOTIMG)
 	@echo ----- Made recovery image -------- $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 
