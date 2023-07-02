370b0aea956205241fbe1849a0553d775b0f1ac6
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-07-02 22:12:24 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,7 +2,7 @@ DEVICE_PATH := device/Micromax/A109
 
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex
+TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 
