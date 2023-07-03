ef98261ab459333e779f5f229de98d9e79decf1b
HemanthJabalpuri <hemanthjabalpuri@gmail.com>
2023-07-03 15:39:08 +0530

    add brightness path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,3 +26,5 @@ BOARD_FLASH_BLOCK_SIZE := 512
 # TWRP
 TW_THEME := portrait_mdpi
 RECOVERY_SDCARD_ON_DATA := true
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
